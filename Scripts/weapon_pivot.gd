a6f2793c01a0262867d3dcb602b49db89540a5bc
jkkiveko <jameskk2004@gmail.com>
2025-09-05 18:30:32 -0400

    Delete delta and also add folders idk

--- Scripts/weapon_pivot.gd
+++ Scripts/weapon_pivot.gd
@@ -1,14 +1,14 @@
 class_name WeaponPivot extends RigidBody2D
 
 ## How much force the weapon rotates with.
-@export var shwing_force : float = 10000000
+@export var shwing_force : float = 200000
 ## Revs per sex
 @export var max_rotation_speed : float = 2
 
-func _process(delta):
+func _process(_delta):
 	var current_rps = angular_velocity / (2 * PI)
 	if Input.is_action_pressed("Swing Clockwise") and current_rps < max_rotation_speed:
-		apply_torque(shwing_force * delta)
+		apply_torque(shwing_force)
 	elif Input.is_action_pressed("Swing Counterclockwise") and -current_rps < max_rotation_speed:
-		apply_torque(-shwing_force * delta)
+		apply_torque(-shwing_force)
 	
