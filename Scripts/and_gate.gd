930fdaab10722bee71b06f9d92141c8ae3f800ac
Nathan Harrison <52888031+nateCode1@users.noreply.github.com>
2025-09-07 15:12:02 -0400

    buttons for james

--- Scripts/and_gate.gd
+++ Scripts/and_gate.gd
@@ -3,17 +3,27 @@ extends Powerable
 @export var connects_to: Array[Powerable]
 ## The number of buttons concurrently powering this and gate required for it to activate
 @export var required_power: int = 2
+@export var unpowered_colour: Color = Color(1.0,1.0,1.0,0.3)
+@export var powered_colour: Color = Color(1.0,1.0,1.0,1.0)
 
 var current_power: int = 0
+
+func _ready() -> void:
+	if "modulate" in self:
+		modulate = unpowered_colour
 
 func on_recieve_power() -> void:
 	current_power += 1
 	if current_power - 1 < required_power and current_power >= required_power:
+		if "modulate" in self:
+			modulate = powered_colour
 		for i in connects_to:
 			i.on_recieve_power()
 
 func on_lose_power() -> void:
 	current_power -= 1
 	if current_power + 1 >= required_power and current_power < required_power:
+		if "modulate" in self:
+			modulate = unpowered_colour
 		for i in connects_to:
 			i.on_lose_power()
