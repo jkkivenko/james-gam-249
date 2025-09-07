68d64c7d5bbf257f622a6c5f5063b2ad690538e1
Nathan Harrison <52888031+nateCode1@users.noreply.github.com>
2025-09-07 16:37:01 -0400

    Fix stuff related to new sprites

--- Scripts/gügly_eye_controller.gd
+++ Scripts/gügly_eye_controller.gd
@@ -1,17 +1,28 @@
-extends Node2D
+class_name Googly extends Node2D
 
-const pixel_radius: float = 50
+@export var dead: bool = false
+
+const pixel_radius: float = 1.5
 const max_pupil_velocity = 400
-@onready var pupil = $Sprite2D
-@onready var child_radius = pupil.scale.x * pixel_radius # assumes uniform scale
+@onready var pupil = $Pupil
+@onready var x = $x
+@onready var child_radius = pupil.scale.x * (pixel_radius / 3) # assumes uniform scale
 @onready var max_dist_from_center = pixel_radius - child_radius
 
 var pupil_velocity = Vector2(0.0, 0.0)
 @onready var prev_pos = global_position
 
 func _process(delta: float):
+	if dead:
+		pupil.visible = false
+		x.visible = true
+	else:
+		pupil.visible = true
+		x.visible = false
+	
 	#if Input.is_action_pressed("Move Left"):
 		#pupil.position = Vector2(0,0)
+	#print(max_dist_from_center)
 		
 	#position = get_global_mouse_position()
 	
